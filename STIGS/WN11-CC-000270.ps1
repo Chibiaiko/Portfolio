27d5e11215c4ca95f8166ffe5db785a5628a9e91
Alexis <112157311+Chibiaiko@users.noreply.github.com>
2025-10-27 15:48:17 +0900

    Create WN11-CC-000270.ps1

---
+++ STIGS/WN11-CC-000270.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    Prevents the saving of Remote Desktop passwords by setting "DisablePasswordSaving" to 1.
+
+.NOTES
+    Author          : Alexis McGuire
+    LinkedIn        : linkedin.com/in/alexismcguire1/
+    GitHub          : github.com/Chibiaiko
+    Date Created    : 2025-10-27
+    Last Modified   : 2025-10-27
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000270
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Disables the saving of Remote Desktop passwords to enhance credential security.
+    Example:
+    PS C:\> .\WN11-CC-000270_DisablePasswordSaving.ps1
+#>
+
+# Set DisablePasswordSaving to 1 (Do not allow passwords to be saved)
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+$regName = "DisablePasswordSaving"
+$regValue = 1
+
+# Create the key if it doesn't exist
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the value
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type DWord
+
+# Verify
+Get-ItemProperty -Path $regPath -Name $regName
