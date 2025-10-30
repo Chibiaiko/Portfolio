1f2bbc921e58335cfd11664da61cbe8021d25de9
Alexis <112157311+Chibiaiko@users.noreply.github.com>
2025-10-31 08:41:23 +0900

    Update WIN11-00-000175.ps1

--- STIGS/WIN11-00-000175.ps1
+++ STIGS/WIN11-00-000175.ps1
@@ -14,11 +14,11 @@ Plugin IDs   : N/A
 STIG-ID      : WIN11-00-000175
 
 .TESTED ON
-Date(s) Tested :
-Tested By :
-Systems Tested :
-PowerShell Ver. :
-
+    Date(s) Tested  : 2025-10-30
+    Tested By       : Alexis McGuire
+    Systems Tested  : Windows 11 (PowerShell 5.1)
+    PowerShell Ver. : Windows PowerShell 5.1 (ISE)
+    
 .USAGE
 Put any usage instructions here.
 
