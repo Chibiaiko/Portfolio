cd41b604e2fb8ee47915b0242b0e179272c9044a
Alexis <112157311+Chibiaiko@users.noreply.github.com>
2025-10-31 09:06:52 +0900

    Update WN11-CC-000205.ps1

--- STIGS/WN11-CC-000205.ps1
+++ STIGS/WN11-CC-000205.ps1
@@ -1,17 +1,17 @@
 <#
 .SYNOPSIS
-  Apply STIG WN11-CC-000205: set HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection\AllowTelemetry to 0 (Security) or 1 (Basic).
+    Apply STIG WN11-CC-000205: set HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection\AllowTelemetry to 0 (Security) or 1 (Basic).
 
 .NOTES
-Author : Alexis McGuire
-LinkedIn : linkedin.com/in/alexismcguire1/
-GitHub : github.com/Chibiaiko
-Date Created : 2025-10-24
-Last Modified : 2025-10-24
-Version : 1.0
-CVEs : N/A
-Plugin IDs : N/A
-STIG-ID : WN11-CC-000205
+Author         : Alexis McGuire
+LinkedIn       : linkedin.com/in/alexismcguire1/
+GitHub         : github.com/Chibiaiko
+Date Created   : 2025-10-24
+Last Modified  : 2025-10-24
+Version        : 1.0
+CVEs           : N/A
+Plugin IDs     : N/A
+STIG-ID        : WN11-CC-000205
 
 .TESTED ON
     Date(s) Tested  : 2025-10-24
@@ -20,7 +20,7 @@ STIG-ID : WN11-CC-000205
     PowerShell Ver. : Windows PowerShell 5.1 (ISE)
 
 .USAGE
-Put any usage instructions here.
+  Put any usage instructions here.
 
 Example syntax:
 PS C:\> .\WN11_CC_000205.ps1 -Level 0
